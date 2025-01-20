50f4bbaeff8ffb791ae60b22e0d8bc096811fd7a
oldpondplop <>
2025-01-20 17:38:20 +0200

    config

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,20 +1,16 @@
-import os
-
 from pydantic_settings import BaseSettings
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Settings(BaseSettings):
-  DATABASE_URL: str = os.getenv("DATABASE_URL")
-  FRONTEND_URL: str = os.getenv("FRONTEND_URL")
-  DEBUG: bool = os.getenv("DEBUG", "False").lower() in ("true", "1")
-  # auth
-  SECRET_KEY: str = os.getenv("SECRET_KEY")
-  ALGORITHM: str = os.getenv("HS256")
-  ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+  DATABASE_URL: str
+  FRONTEND_URL: str
+  DEBUG: bool = False
+  # Auth
+  SECRET_KEY: str
+  ALGORITHM: str = "HS256"
+  ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
   class Config:
     env_file = ".env"
+
 
 settings = Settings()
