df94428aa81ea6176961601b6778b6668c0ffe74
oldpondplop <>
2025-01-20 16:13:12 +0200

    config

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,12 +6,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
-    FRONTEND_URL: str = os.getenv("FRONTEND_URL")
-    DEBUG: bool = os.getenv("DEBUG", "False").lower() in ("true", "1")
-    # auth
-    SECRET_KEY: str = os.getenv("SECRET_KEY")
-    ALGORITHM: str = os.getenv("HS256")
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+  DATABASE_URL: str = os.getenv("DATABASE_URL")
+  FRONTEND_URL: str = os.getenv("FRONTEND_URL")
+  DEBUG: bool = os.getenv("DEBUG", "False").lower() in ("true", "1")
+  # auth
+  SECRET_KEY: str = os.getenv("SECRET_KEY")
+  ALGORITHM: str = os.getenv("HS256")
+  ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+  class Config:
+    env_file = ".env"
 
 settings = Settings()
